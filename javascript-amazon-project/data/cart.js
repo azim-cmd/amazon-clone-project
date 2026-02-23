e7cf3543d53ffc23328dd68e9290c2a65b84f563
azimuddin <azimnm99@gmail.com>
2026-02-23 12:52:39 +0530

    removing item button error fixed

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -24,7 +24,7 @@ export function addToCart(productName){
     }
 }
 
-export function removeFromCart(){
+export function removeFromCart(productId){
   const newCart = [];
 
   cart.forEach((cartItem) =>{
