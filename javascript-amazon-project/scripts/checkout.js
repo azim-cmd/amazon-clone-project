5f548c19686c0d38aab180364ae5232d20236489
azimuddin <azimnm99@gmail.com>
2026-03-02 17:31:01 +0530

    delete items when delete button clicked

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -20,7 +20,7 @@ cart.forEach((cartItem) =>{
   console.log(matchingProduct);
 
 cartSummartyHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -104,6 +104,11 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () =>{
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    console.log(container);
+    container.remove();
   });
 });
