b504aec5fac31af223843900cd54655b7d359d88
azimuddin <azimnm99@gmail.com>
2026-04-26 16:15:04 +0530

    bug fixing for error in Checkout page

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -2,10 +2,6 @@ import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-hello();
-
-console.log(dayjs());
-
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
